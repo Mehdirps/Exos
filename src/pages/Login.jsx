c5ad2edd9006bd82a2ad8e04b310266ab20b6ca9
Mehdirps <mehdi.raposo77@gmail.com>
2023-08-24 14:53:33 +0200

    🎨 up : exo 11

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-
+    const [error, setError] = useState('');
     const navigate = useNavigate();
     return (
         <div>
             Login
+            {
+                error !== '' ?
+                    <p style={{ color: 'red' }}>{error}</p>
+                    : null
+            }
             <form action="" onSubmit={(e) => {
                 e.preventDefault();
 
-                if (e.target.name.value !== '' && e.target.password.value !== '') {
+                if (e.target.name.value === 'cloudcampus' && e.target.password.value === '0000') {
                     return navigate('/tasklist');
+                } else {
+                    setError('Identifants incorrects')
                 }
 
             }}>
